43f1a07ad50bdf2826b4ea4ead9a65d2a1f2c2fa
Edwin Rozo Gómez <erozog125@gmail.com>
2021-10-25 14:51:32 -0500

    Modificacion en algunos estilos

--- Carrito de compras/js/main.js
+++ Carrito de compras/js/main.js
@@ -1,178 +1,180 @@
-
 const products = [
-    {
-        id: 1,
-        name: 'Arroz',
-        price: 2000,
-        image: 'images/arroz.webp'
-    },
-    {
-        id: 2,
-        name: 'frijoles',
-        price: 2200,
-        image: 'images/frijol.jpg'
-    },
-    {
-        id: 3,
-        name: 'Garvanzos',
-        price: 2000,
-        image: 'images/garbanzos.jpg'
-    },
-    {
-        id: 4,
-        name: 'Alverjas',
-        price: 2200,
-        image: 'images/alverja.jpg'
-    }
+ {
+  id: 1,
+  name: "Arroz",
+  price: 2000,
+  image: "images/arroz.webp",
+ },
+ {
+  id: 2,
+  name: "frijoles",
+  price: 2200,
+  image: "images/frijol.jpg",
+ },
+ {
+  id: 3,
+  name: "Garvanzos",
+  price: 2000,
+  image: "images/garbanzos.jpg",
+ },
+ {
+  id: 4,
+  name: "Arvejas",
+  price: 2200,
+  image: "images/alverja.jpg",
+ },
+ {
+  id: 5,
+  name: "Atún",
+  price: 2200,
+  image: "images/atun.png",
+ },
+ {
+  id: 6,
+  name: "Pastas",
+  price: 2200,
+  image: "images/pasta.jpg",
+ },
 ];
 
 let carrito = [];
 
-const container = document.querySelector('.container_prducts');
-const container_cart = document.querySelector('.container_cart');
-const cont_products = document.querySelector('.cont');
-const container_btn_cart = document.querySelector('.cart');
+const container = document.querySelector(".container_prducts");
+const container_cart = document.querySelector(".container_cart");
+const cont_products = document.querySelector(".cont");
+const container_btn_cart = document.querySelector(".cart");
 
 function renderizarProductos() {
-    products.forEach((info) => {
-        const card = document.createElement('div');
-        const container_image_product = document.createElement('div');
-        const container_info_product = document.createElement('div');
-        const image_product = document.createElement('img');
-        const name_product = document.createElement('h2');
-        const price = document.createElement('p');
+ products.forEach((info) => {
+  const card = document.createElement("div");
+  const container_image_product = document.createElement("div");
+  const container_info_product = document.createElement("div");
+  const image_product = document.createElement("img");
+  const name_product = document.createElement("h2");
+  const price = document.createElement("p");
 
-        const add_product_cart = document.createElement('button');
+  const add_product_cart = document.createElement("button");
 
-        add_product_cart.textContent = 'Agregar al carrito';
-        name_product.textContent = info.name;
-        price.textContent = info.price;
-        image_product.setAttribute('src', info.image);                   
-        add_product_cart.setAttribute('marcador', info.id);
-        add_product_cart.addEventListener('click', addCart);
-        card.classList.add('card');
-        container_image_product.classList.add('container_image_product');
-        add_product_cart.classList.add('btn_add_cart');
-        container_info_product.classList.add('container_info_product');
+  add_product_cart.textContent = "Agregar al carrito";
+  name_product.textContent = info.name;
+  price.textContent = info.price;
+  image_product.setAttribute("src", info.image);
+  add_product_cart.setAttribute("marcador", info.id);
+  add_product_cart.addEventListener("click", addCart);
+  card.classList.add("card");
+  container_image_product.classList.add("container_image_product");
+  add_product_cart.classList.add("btn_add_cart");
+  container_info_product.classList.add("container_info_product");
 
-        container_image_product.appendChild(image_product);
-        container_info_product.appendChild(name_product);
-        container_info_product.appendChild(price);
-        container_info_product.appendChild(add_product_cart);
-        card.appendChild(container_image_product);
-        card.appendChild(container_info_product);
-        container.appendChild(card);           
-    });
+  container_image_product.appendChild(image_product);
+  container_info_product.appendChild(name_product);
+  container_info_product.appendChild(price);
+  container_info_product.appendChild(add_product_cart);
+  card.appendChild(container_image_product);
+  card.appendChild(container_info_product);
+  container.appendChild(card);
+ });
 }
 
 function addCart(e) {
-    carrito.push(e.target.getAttribute('marcador'));
-    renderCart();         
+ carrito.push(e.target.getAttribute("marcador"));
+ renderCart();
 }
 
 function clearCart() {
-    carrito = [];
-    renderCart();
+ carrito = [];
+ renderCart();
 }
 
-function renderCart() {               
-    container_cart.innerHTML = '';
-    const reset_cart = [...new Set(carrito)];
-    cont_products.textContent = reset_cart.length;
+function renderCart() {
+ container_cart.innerHTML = "";
+ const reset_cart = [...new Set(carrito)];
+ cont_products.textContent = reset_cart.length;
 
+ reset_cart.forEach((item) => {
+  const items = products.filter((item_products) => {
+   return item_products.id === parseInt(item);
+  });
 
-    reset_cart.forEach((item) => {
+  const num_products = carrito.reduce((total, id_product) => {
+   return id_product === item ? (total += 1) : total;
+  }, 0);
 
-        const items = products.filter((item_products) => {                        
-            return item_products.id === parseInt(item);
-        });
+  const card_product = document.createElement("div");
+  const image_prduct = document.createElement("img");
+  const name = document.createElement("p");
+  const price = document.createElement("p");
+  const cuanty = document.createElement("p");
+  const btn_add = document.createElement("button");
+  const btn_remove = document.createElement("button");
+  const btn_remove_product = document.createElement("button");
 
+  card_product.classList.add("card_product_cart");
+  btn_remove.classList.add("btn_remove");
+  btn_add.classList.add("btn_add");
+  btn_remove_product.classList.add("btn_remove_element");
+  image_prduct.setAttribute("src", items[0].image);
+  name.textContent = items[0].name;
+  price.textContent = items[0].price;
+  cuanty.textContent = num_products;
+  btn_add.textContent = "+";
+  btn_add.setAttribute("marcador", items[0].id);
+  btn_remove.textContent = "-";
+  btn_remove_product.textContent = "X";
 
-        const num_products = carrito.reduce((total, id_product) => {                
-            return id_product === item ? 
-            total += 1 
-            : 
-            total;
-        }, 0);
+  card_product.appendChild(image_prduct);
+  card_product.appendChild(name);
+  card_product.appendChild(price);
+  card_product.appendChild(cuanty);
+  card_product.appendChild(btn_add);
+  card_product.appendChild(btn_remove);
+  card_product.appendChild(btn_remove_product);
 
-        const card_product = document.createElement('div');
-        const image_prduct = document.createElement('img');
-        const name = document.createElement('p');
-        const price = document.createElement('p');
-        const cuanty = document.createElement('p');
-        const btn_add = document.createElement('button');
-        const btn_remove = document.createElement('button');
-        const btn_remove_product = document.createElement('button');
+  container_cart.appendChild(card_product);
 
-        card_product.classList.add('card_product_cart');
-        btn_remove.classList.add('btn_remove');
-        btn_add.classList.add('btn_add');
-        btn_remove_product.classList.add('btn_remove_element')
-        image_prduct.setAttribute('src', items[0].image);
-        name.textContent = items[0].name;
-        price.textContent = items[0].price;
-        cuanty.textContent = num_products;
-        btn_add.textContent = '+';
-        btn_add.setAttribute('marcador', items[0].id);
-        btn_remove.textContent = '-';
-        btn_remove_product.textContent = 'X';
+  btn_remove_product.dataset.item = item;
+  btn_remove.dataset.item = item;
 
-        
-        card_product.appendChild(image_prduct);
-        card_product.appendChild(name);
-        card_product.appendChild(price);
-        card_product.appendChild(cuanty);
-        card_product.appendChild(btn_add);
-        card_product.appendChild(btn_remove);
-        card_product.appendChild(btn_remove_product)
+  btn_add.addEventListener("click", add);
+  btn_remove.addEventListener("click", remove);
+  btn_add.addEventListener("click", removeProduct);
+  btn_remove_product.addEventListener("click", removeProduct);
+ });
 
-        container_cart.appendChild(card_product);
+ const clear_cart = document.createElement("button");
+ clear_cart.classList.add("btn_clear_cart");
 
-        btn_remove_product.dataset.item = item;
-        btn_remove.dataset.item = item;
+ clear_cart.textContent = "Limpiar carrito";
+ // clear_cart.dataset.item = item;
+ container_cart.appendChild(clear_cart);
 
-        btn_add.addEventListener('click', add);
-        btn_remove.addEventListener('click', remove);
-        btn_add.addEventListener('click', removeProduct);
-        btn_remove_product.addEventListener('click', removeProduct)
-    });
-
-    const clear_cart = document.createElement('button');
-    clear_cart.classList.add('btn_clear_cart');
-        
-    clear_cart.textContent = 'Limpiar carrito';
-    // clear_cart.dataset.item = item;
-    container_cart.appendChild(clear_cart);
-
-
-    clear_cart.addEventListener('click',clearCart)
+ clear_cart.addEventListener("click", clearCart);
 }
 
 function add(e) {
-    carrito.push(e.target.getAttribute('marcador'));
-    renderCart();   
+ carrito.push(e.target.getAttribute("marcador"));
+ renderCart();
 }
-    
+
 function remove(e) {
-    const id = e.target.dataset.item;             
-    let index = carrito.indexOf(id);
-    console.log(carrito);
-    console.log(index);
-    carrito.splice(index, 1)
-    renderCart()
+ const id = e.target.dataset.item;
+ let index = carrito.indexOf(id);
+ console.log(carrito);
+ console.log(index);
+ carrito.splice(index, 1);
+ renderCart();
 }
 
 function removeProduct(e) {
-    const id = e.target.dataset.item;
-    carrito = carrito.filter((carritoId)=> {
-        return carritoId !== id;
-    });
-    renderCart();
+ const id = e.target.dataset.item;
+ carrito = carrito.filter((carritoId) => {
+  return carritoId !== id;
+ });
+ renderCart();
 }
 
-container_btn_cart.addEventListener('mouseover', ()=> {
-    container_cart.style.display = 'block';
-})
+container_btn_cart.addEventListener("mouseover", () => {
+ container_cart.style.display = "block";
+});
 
 renderizarProductos();
-
